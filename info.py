27f9ff833ce491a730e4bec4e8806a40c5d86bf8
shreyash11-B <shreyashhivre9@gmail.com>
2026-08-08 16:58:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -120,9 +120,9 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 # Channel & Group Links Configuration
 # ============================
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/technokrrish') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/technokrrishyt') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/chatmoviearchive') # Update channel link for the bot
+GRP_LNK = environ.get('GRP_LNK', 'https://t.me/chatmoviearchive') # Group link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/shreyashhivre') # Owner link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/tmaupdatess') # Update channel link for the bot
 
 # ============================
 # User Configuration
